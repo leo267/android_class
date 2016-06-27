9a103bceadd865e35bb091edec3bb22270fa416a
leo267 <leo.asclass267@gmail.com>
2016-06-27 22:04:49 +0800

    get Static Image View From Google Map API

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,11 +1,27 @@
 package com.example.user.simpleui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
 
 public class OrderDetailActivity extends AppCompatActivity {
+
+    TextView noteTextView;
+    TextView menuResultsTextView;
+    TextView storeInfoTextView;
+    ImageView staticMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +34,90 @@ public class OrderDetailActivity extends AppCompatActivity {
         String menuResults = intent.getStringExtra("menuResults");
         String storeInfo = intent.getStringExtra("storeInfo");
 
-        Log.d("Debug", note);
-        Log.d("Debug", menuResults);
-        Log.d("Debug", storeInfo);
+        noteTextView = (TextView) findViewById(R.id.noteTextView);
+        storeInfoTextView = (TextView) findViewById(R.id.storeInfoTextView);
+        menuResultsTextView = (TextView) findViewById(R.id.menuResultsTextView);
+        staticMap = (ImageView) findViewById(R.id.imageView);
 
+        //設定data前，最好先檢查資料是不是null
+        if (note != null)
+            noteTextView.setText(note);
+
+        if (storeInfo != null)
+            storeInfoTextView.setText(storeInfo);
+
+        //先命名一個String , 將句子加上去
+        String text = "";
+        if (menuResults != null) {
+            //資料需要進行轉換
+            try {
+                JSONArray array = new JSONArray(menuResults);
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject object = array.getJSONObject(i);
+                    //先把杯數轉成String
+                    String lNumber = String.valueOf(object.getInt("lNumber"));
+                    String mNumber = String.valueOf(object.getInt("mNumber"));
+                    text += object.getString("drinkName") + ":大杯" + lNumber + "杯  中杯" + mNumber + "杯\n";
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            menuResultsTextView.setText(text);
+
+            //先取得address
+//            String storeInfos = storeInfo.split(",")[1];   //[0]店名、[1]地址
+            String[] storeInfos = storeInfo.split(",");
+            if (storeInfos != null && storeInfos.length > 1) {
+                String address = storeInfos[1];//[0]店名、[1]地址
+                Log.d("Debug", address);
+                //開另外一隻AsyncTask去做query的動作
+                (new GeoCodingTask(staticMap)).execute(address);
+            }
+
+            //開另外一隻Thread去做query的動作
+            //Thread缺點，(不知道執行到什麼時候會結束)會造成memory leak , 因此他不會被 gc (回收) ，Thread會綁住Activity，而Activity可能會綁住View，造成資源無法釋放掉，memory lead產生。
+            //寫thread不要用暱名函式去寫
+            /*new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            }).run();
+*/
+        }
+    }
+
+    private static class GeoCodingTask extends AsyncTask<String, Void, Bitmap> {
+        // 用WeakReference去指
+        WeakReference<ImageView> imageViewWeakReference;
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            //先拿到address
+            String address = params[0];
+            double[] latlng = Utils.getLatLngFromGoogleMapAPI(address);
+            if (latlng != null) {
+                //Log.d("Debug", latlng.toString());
+                Log.d("Debug", String.valueOf(latlng[0]));
+                Log.d("Debug", String.valueOf(latlng[1]));
+            }
+            return Utils.getStaticMap(latlng);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            //如果View還在，就把Bitmap設定上去
+            if(imageViewWeakReference.get() != null){
+                ImageView imageView = imageViewWeakReference.get();
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+
+        // Construct
+        public GeoCodingTask(ImageView imageView){
+            // weak reference 指向 imageView
+            this.imageViewWeakReference = new WeakReference<ImageView>(imageView);
+        }
     }
 }
--- SimpleUI/app/src/main/java/com/example/user/simpleui/Utils.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/Utils.java
@@ -1,11 +1,24 @@
 package com.example.user.simpleui;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * Created by user on 2016/6/20.
@@ -39,4 +52,91 @@ public class Utils {
 
         return ""; //表示失敗,回傳一個空字串
     }
+
+    //取得json的byte檔
+    public static byte[] urlToBytes(String urlString) {
+        // Url ex: http://maps.google.com/maps/api/geocode/json?address=台北市大安區羅斯福路四段一號
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            //讀進buffer裡面
+            InputStream inputStream = connection.getInputStream();
+            //等同於一個buffer
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = inputStream.read(buffer)) != -1) {
+                // 持續寫進 output array stream
+                byteArrayOutputStream.write(buffer, 0, len);
+            }
+            return byteArrayOutputStream.toByteArray();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 回傳精度、緯度，應該是回傳double array
+    public static double[] getLatLngFromGoogleMapAPI(String address) {
+        try {
+            address = URLEncoder.encode(address, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        //串進API裡面
+        String apiURL = "http://maps.google.com/maps/api/geocode/json?address=" + address;
+
+        //url轉成bytes
+        byte[] bytes = Utils.urlToBytes(apiURL);
+
+        //先檢查bytes是否null
+        if (bytes == null) return null;
+
+        //先轉成字串
+        String result = new String(bytes);
+
+        //轉成JSONObject
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+
+            //檢查status是否ok
+            if (jsonObject.getString("status").equals("OK")) {
+                // 拿到location (內含經緯度資訊)
+                JSONObject location = jsonObject.getJSONArray("results")
+                        .getJSONObject(0)
+                        .getJSONObject("geometry")
+                        .getJSONObject("location");
+                double lat = location.getDouble("lat");
+                double lng = location.getDouble("lng");
+
+                //回傳經緯度
+                return new double[]{lat, lng};
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //取得過程中發生錯誤，回傳null。
+        return null;
+    }
+
+
+    public static Bitmap getStaticMap(double[] latlng) {
+        //字串整理
+        String center = String.valueOf(latlng[0]) + "," + String.valueOf(latlng[1]);
+        //串進api
+        String staticMapUrl = "http://maps.google.com/maps/api/staticmap?center=" + center + "&size=640x400&zoom=17";
+
+        byte[] bytes = Utils.urlToBytes(staticMapUrl);
+        if (bytes != null) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            return bitmap;
+        }
+        return null;
+    }
+
 }
